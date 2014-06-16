176ef041f4b443109aa2909ac6a4cb1e4a67790e
Vickchelo <vickchelo16@outlook.com>
2014-06-15 23:57:23 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -4,13 +4,17 @@ var player1 = new Array(6);
 var player2 = new Array(6); 
 var cont=0; 
 var ficha;
-var array_to_fill = "";
+var array_to_fill = ""; 
 function distribute(){ 
     duplicate_array(); 
 	delivering_tokens(player1);
     delivering_tokens(player2);
     publish_array(player1);
     publish_array(player2);
+   var first_player = evaluate_mula(66); 
+	send_to_android();
+	console.log("Tokens rests :" + tokens_filled);
+	start_game(first_player);
 }
 function delivering_tokens(to_deliver){
 	for(var i = 0; i < 6; i++){ 
@@ -32,12 +36,52 @@ function duplicate_array(){
 function publish_array(to_display){ 
    for(var i = 0; i < 6; i++){  
    var word = to_display[i] ;
+   // CREATE TOKENS WITH IMAGES AND SEND TO PLAYERS
    console.log(word);
    if(word == "undefined"){
  	console.log(to_display + "- Position = " + i + ", Value = " + to_display[i]);
    	} 
    }
    console.log("-------------------------------");
+}
+function evaluate_mula(to_find){	 
+	for(var j = 0; j <= 6; j++){
+		var mula = player1[j];
+		var mula2 = player2[j];
+		console.log("Posible mula 1: "+ mula)
+		console.log("Posible mula 2: "+ mula2)
+		if(mula == to_find ){
+			console.log("Start player1 with :" + mula);
+			return "player1"; 
+		}	
+		else if(mula2 == to_find){
+			console.log("Start player2 with: "+ mula2);
+			return "player2";	 
+		}
+	}
+	evaluate_mula(to_find-11)
+	console.log("Mula to find : " + to_find);
+}
+function send_to_android(){
+	
+}
+function start_game(who_start){ 
+	 alert(who_start); 
+	if(who_start == "player1"){
+		// GIVE READY EVENT TO ANDROID
+		// RECEIVE TOKEN AND PLACE ON CANVAS 
+		// USE DRAW FUNCTION TO PLACE IT
+		console.log("TYPE YOUR PLAYER");
+		var player1 = console.read;
+		var oldf = console.read; 
+	}
+	if (who_start == "player2"){
+		console.log("TYPE YOUR PLAYER 2");
+		var player1 = console.read;
+		var oldf = console.read; 
+		alert(player1);
+	}
+	
 }
 function random(low, high) { 
    return Math.floor(Math.random() * (high - low) + low);
@@ -69,7 +113,7 @@ var canvas = document.getElementById('canvas1');
 	ctx.drawImage(img,x,50,50,50); 
 	}
 	var looptimer = setTimeout('draw('+x+','+y+')',50); 
-	 
+
 }
 function place_token(x){
  
